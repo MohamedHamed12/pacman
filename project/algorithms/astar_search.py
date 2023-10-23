ab1527681814d090a5e0c325bf6f5fccb0836ff1
MohamedHamed12 <mh1779371@gmail.com>
2023-10-23 12:24:38 +0300

    astar_search

---
+++ project/algorithms/astar_search.py
@@ -0,0 +1,55 @@
+
+import heapq
+
+
+class CustomPriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, item, priority):
+        heapq.heappush(self.heap, (priority, item))
+
+    def pop(self):
+        priority, item = heapq.heappop(self.heap)
+        return (item, priority)
+
+    def size(self):
+        return len(self.heap)
+
+
+def astar_search(problem, heuristic_func):
+    frontier = CustomPriorityQueue()
+    explored = set()
+    parent = {}
+
+    start = problem.getStartState()
+    frontier.push(start, 0)
+
+    while frontier.size():
+        current_state, current_cost = frontier.pop()
+
+        if problem.isGoalState(current_state):
+            path, total_cost = get_path(current_state, parent)
+            return path
+
+        explored.add(current_state)
+
+        for new_state, new_action, new_cost in problem.getSuccessors(current_state):
+            if new_state in explored:
+                continue
+            total_cost = current_cost + new_cost
+            heuristic_cost = heuristic_func(new_state, problem)
+            frontier.push(new_state, total_cost+heuristic_cost)
+            explored.add(new_state)
+            parent[new_state] = (current_state, new_action, new_cost)
+    return None
+
+
+def get_path(cur_state, parent):
+    total_cost = 0
+    path = []
+    while cur_state in parent:
+        cur_state, action, cost = parent[cur_state]
+        path.append(action)
+        total_cost += cost
+    return path[::-1], total_cost
--- project/search.py
+++ project/search.py
@@ -110,6 +110,8 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    from algorithms.astar_search import astar_search
+    return astar_search(problem, heuristic)
     util.raiseNotDefined()
 
 
